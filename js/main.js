2dfcc8db470bfa30a4c8526fa5e4c76f6cdce296
Yuriy Tihanovich <yura477573@mail.ru>
2021-09-28 10:41:24 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+let numberOfFilms = prompt('Сколько фильмов вы посмотрели?', "");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+}
