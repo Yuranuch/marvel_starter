e2954a9f89d3709922b9d55f512aa9a3e2997c87
Yuriy Tihanovich <yura477573@mail.ru>
2021-09-28 13:21:17 +0300

    done third task

--- js/main.js
+++ js/main.js
@@ -1,21 +1,63 @@
 
-let numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+let numberOfFilms;
+function start () {
+    numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+    while(numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms) ){
+        numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+    }
+
+}
+start()
 
 let personalMovieDB = {
     count: numberOfFilms,
+    status: null,
     movies: {},
     actors: {},
-    genres: {},
-    privat: false
+    genres: [],
+    privat: false,
 }
 
-console.log(personalMovieDB);
-for ( let i = 0; i < numberOfFilms; i++) {
-    let firstQuestion = prompt('Какой фильм вы посмотрели?', '');
-    let secondQuestion = prompt('На сколько вы его оцените?', '');
-    if((firstQuestion === null || secondQuestion === null) || (firstQuestion === "" || secondQuestion === "") || firstQuestion.length > 20) {
-        --i
-    }else {
-        personalMovieDB.movies[firstQuestion] = secondQuestion;
+function rememberMyFilms () {
+    for ( let i = 0; i < numberOfFilms; i++) {
+        let firstQuestion = prompt('Какой фильм вы посмотрели?', '');
+        let secondQuestion = prompt('На сколько вы его оцените?', '');
+        if((firstQuestion === null || secondQuestion === null) || (firstQuestion === "" || secondQuestion === "") || firstQuestion.length > 20) {
+            --i
+        }else {
+            personalMovieDB.movies[firstQuestion] = secondQuestion;
+        }
     }
 }
+rememberMyFilms();
+
+function confirmLevel () {
+    if(numberOfFilms <= 3) {
+        personalMovieDB.status = 'beginer'
+    }
+    else if (numberOfFilms >= 4 && numberOfFilms <= 7 ){
+        personalMovieDB.status = 'middle'
+    }
+    else {
+        personalMovieDB.status = 'veteran'
+    }
+}
+confirmLevel();
+function writeYourGenres () {
+    for (let i = 1; i <= 3; i++){
+        let ganresQuestion = prompt(`Ваш любимый жанр номер - ${i}`);
+        personalMovieDB.genres[i - 1] = ganresQuestion;
+    }
+}
+writeYourGenres();
+
+function showMyBd (hidden) {
+    if(!hidden) {
+        console.log(personalMovieDB);
+    }
+    else {
+        console.log('It is Private Object!!!')
+    }
+}
+showMyBd(personalMovieDB.privat)
+
