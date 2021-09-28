3374009c316d81831d3cb28142106a45e1f0607e
Yuriy Tihanovich <yura477573@mail.ru>
2021-09-28 11:06:59 +0300

    complete first task

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-let numberOfFilms = prompt('Сколько фильмов вы посмотрели?', "");
+let numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -8,3 +8,9 @@ let personalMovieDB = {
     genres: {},
     privat: false
 }
+
+let firstQuestion = prompt('Какой фильм вы посмотрели?', '');
+let secondQuestion = prompt('На сколько вы его оцените?', '');
+
+personalMovieDB.movies[firstQuestion] = secondQuestion;
+console.log(personalMovieDB);
