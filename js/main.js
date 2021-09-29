b532ba46b07bbebeebde5cd7ae5481df0cf8a886
Yuriy Tihanovich <yura477573@mail.ru>
2021-09-29 12:10:13 +0300

    done third task

--- js/main.js
+++ js/main.js
@@ -1,75 +1,4 @@
-//
-// let numberOfFilms;
-// function start () {
-//     numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
-//     while(numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms) ){
-//         numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
-//     }
-//
-// }
-// start()
-//
-// let personalMovieDB = {
-//     count: numberOfFilms,
-//     status: null,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false,
-// }
-//
-// function rememberMyFilms () {
-//     for ( let i = 0; i < numberOfFilms; i++) {
-//         let firstQuestion = prompt('Какой фильм вы посмотрели?', '');
-//         let secondQuestion = prompt('На сколько вы его оцените?', '');
-//         if((firstQuestion === null || secondQuestion === null) || (firstQuestion === "" || secondQuestion === "") || firstQuestion.length > 20) {
-//             --i
-//         }else {
-//             personalMovieDB.movies[firstQuestion] = secondQuestion;
-//         }
-//     }
-// }
-// rememberMyFilms();
-//
-// function confirmLevel () {
-//     if(numberOfFilms <= 3) {
-//         personalMovieDB.status = 'beginer'
-//     }
-//     else if (numberOfFilms >= 4 && numberOfFilms <= 7 ){
-//         personalMovieDB.status = 'middle'
-//     }
-//     else {
-//         personalMovieDB.status = 'veteran'
-//     }
-// }
-// confirmLevel();
-// function writeYourGenres () {
-//     for (let i = 1; i <= 3; i++){
-//         let ganresQuestion = prompt(`Ваш любимый жанр номер - ${i}`);
-//         personalMovieDB.genres[i - 1] = ganresQuestion;
-//     }
-// }
-// writeYourGenres();
-//
-// function showMyBd (hidden) {
-//     if(!hidden) {
-//         console.log(personalMovieDB);
-//     }
-//     else {
-//         console.log('It is Private Object!!!')
-//     }
-// }
-// showMyBd(personalMovieDB.privat)
 
-// function first (text, second) {
-//     console.log(`${text} Hello`);
-//     second()
-//
-// }
-// function second () {
-//     console.log('Yura')
-// }
-// first('O,',second);
 
 // let options = {
 //     name: 'Yura',
@@ -91,3 +20,89 @@
 //     }
 //
 // }
+
+// function first (text, second) {
+//     console.log(`${text} Hello`);
+//     second()
+//
+// }
+// function second () {
+//     console.log('Yura')
+// }
+// first('O,',second);
+
+
+let numberOfFilms;
+let personalMovieDB = {
+    count: numberOfFilms,
+    status: null,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function () {
+        numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+        while(numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms) ){
+            numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+        }
+    },
+    rememberMyFilms: function  () {
+        for ( let i = 0; i < numberOfFilms; i++) {
+            let firstQuestion = prompt('Какой фильм вы посмотрели?', '');
+            let secondQuestion = prompt('На сколько вы его оцените?', '');
+            if((firstQuestion === null || secondQuestion === null) || (firstQuestion === "" || secondQuestion === "") || firstQuestion.length > 20) {
+                --i
+            }else {
+                personalMovieDB.movies[firstQuestion] = secondQuestion;
+             }
+        }
+    },
+    confirmLevel: function  () {
+        if(numberOfFilms <= 3) {
+            personalMovieDB.status = 'beginer'
+        }
+        else if (numberOfFilms >= 4 && numberOfFilms <= 7 ){
+            personalMovieDB.status = 'middle'
+        }
+        else {
+            personalMovieDB.status = 'veteran'
+        }
+    },
+    writeYourGenres: function  () {
+        for (let i = 1; i <= 3; i++){
+            let ganresQuestion = prompt(`Ваш любимый жанр номер - ${i}`);
+            if( ganresQuestion == "" || ganresQuestion == null ) {
+            i--
+            }else {
+                personalMovieDB.genres[i - 1] = ganresQuestion;
+            }
+        }
+    },
+    showMyBd: function  (hidden) {
+        if(!hidden) {
+            console.log(personalMovieDB);
+        }
+        else {
+            console.log('It is Private Object!!!')
+        }
+    },
+    toggleVisibleMyDB: function (){
+            if(personalMovieDB.privat  === false) {
+                personalMovieDB.privat  = true
+                console.log(personalMovieDB.privat)
+                return personalMovieDB.privat
+            }else {
+                personalMovieDB.privat  = false
+                console.log(personalMovieDB.privat)
+            }
+    },
+};
+personalMovieDB.start()
+personalMovieDB.rememberMyFilms();
+personalMovieDB.confirmLevel();
+personalMovieDB.writeYourGenres();
+personalMovieDB.showMyBd(personalMovieDB.privat);
+personalMovieDB.toggleVisibleMyDB();
+console.log(personalMovieDB.privat)
+
+
